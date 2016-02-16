aca526016250dae9000453a00bb2163057d9d3d1
Ciro Santilli <ciro.santilli@savoirfairelinux.com>
2016-02-17 00:19:37 +0100

    echo starting client / starting daemon on ubuntu run script
    Change-Id: Id043a6d6f2c0de1dc0ed394ff306ef441c98b8ef
    Tuleap: #390

--- ubuntu-15.10-run.sh
+++ ubuntu-15.10-run.sh
@@ -2,7 +2,9 @@
 # Run local install daemon and client that have
 # been installed with the install script on the background.
 cd "$(dirname "${BASH_SOURCE[0]}")"
+(echo 'Starting daemon.'; date) >>daemon.log
 ./install/daemon/libexec/dring -c -d >>daemon.log 2>&1 &
 echo $! >daemon.pid
+(echo 'Starting client;'; date) >>client-gnome.log
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:install/lrc/lib" ./install/client-gnome/bin/gnome-ring -d >>client-gnome.log 2>&1 &
 echo $! >client-gnome.pid
