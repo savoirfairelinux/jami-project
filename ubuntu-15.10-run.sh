4189b4d89b25889820df67026fb741557595215d
Ciro Santilli <ciro.santilli@savoirfairelinux.com>
2016-02-17 16:30:45 +0100

    Remove stop script: dring already dies when client quits
    Change-Id: Iae58c74743ddf268d38b3af4604aee53a018c7c0
    Tuleap: #393

--- ubuntu-15.10-run.sh
+++ ubuntu-15.10-run.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 # Run local install daemon and client that have
 # been installed with the install script on the background.
+#
+# Options:
+#
+# - d: run the daemon on a debugger in the foreground.
+set -ex
+debug=false
+while getopts d OPT; do
+  case "$OPT" in
+    d)
+      debug='true'
+    ;;
+    \?)
+      exit 1
+    ;;
+  esac
+done
 cd "$(dirname "${BASH_SOURCE[0]}")"
 (echo 'Starting daemon.'; date) >>daemon.log
-./install/daemon/libexec/dring -c -d >>daemon.log 2>&1 &
-echo $! >daemon.pid
+if ! $debug; then
+  ./install/daemon/libexec/dring -c -d >>daemon.log 2>&1 &
+  echo $! >daemon.pid
+fi
 (echo 'Starting client;'; date) >>client-gnome.log
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:install/lrc/lib" ./install/client-gnome/bin/gnome-ring -d >>client-gnome.log 2>&1 &
 echo $! >client-gnome.pid
+if $debug; then
+  gdb -p "$(cat daemon.pid)" -x gdb.gdb
+fi
