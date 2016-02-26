ce2964aee58c056b2c693096d3b142cd7b047875
aviau <alexandre@alexandreviau.net>
2016-02-26 14:34:41 -0500

    Introduce build script
    Begin merging all helper scripts into one. Support for other distros
    is to be added in the future.
    
    Change-Id: I8ab09502a58ce6a6ccf154be2cf51af401759365

--- ubuntu-15.10-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Run local install daemon and client that have
-# been installed with the install script on the background.
-cd "$(dirname "${BASH_SOURCE[0]}")"
-./install/daemon/libexec/dring -c -d >>daemon.log 2>&1 &
-echo $! >daemon.pid
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:install/lrc/lib" ./install/client-gnome/bin/gnome-ring -d >>client-gnome.log 2>&1 &
-echo $! >client-gnome.pid
