cc95a0fc19688ed3edb679d74c452bff3a114d03
aviau <alexandre@alexandreviau.net>
2016-02-26 14:34:41 -0500

    Introduce ring-build script
    Begin merging all helper scripts into one. Support for other distros
    is to be added in the future.
    
    Change-Id: I8ab09502a58ce6a6ccf154be2cf51af401759365

--- ubuntu-15.10-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Stop local install daemon and client that have been installed with the install script.
-cd "$(dirname "${BASH_SOURCE[0]}")"
-kill "$(cat client-gnome.pid)"
-kill "$(cat daemon.pid)"
