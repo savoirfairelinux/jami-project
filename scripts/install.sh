6c949295d3e1ee460549e753df64775b373cc631
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-03-04 16:56:17 -0500

    add OSX distribution
    - update README
    - add dependencies
    - install script
    - run command
    
    Tuleap: #449
    Change-Id: I43431df02dfa91180d275bcdc0c97e796c6b948b

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,18 +6,23 @@
 
   # -g: install globally instead for all users
   # -s: link everything statically, no D-Bus communication. More likely to work!
+  # -c: client to build
 
 set -ex
 
 global=false
 static=''
-while getopts gs OPT; do
+client=''
+while getopts gsc: OPT; do
   case "$OPT" in
     g)
       global='true'
     ;;
     s)
       static='-DENABLE_STATIC=true'
+    ;;
+    c)
+      client="${OPTARG}"
     ;;
     \?)
       exit 1
@@ -55,9 +60,9 @@ make -j$(nproc)
 cd "${DAEMON}"
 ./autogen.sh
 if $global; then
-  ./configure
+  ./configure $CONFIGURE_FLAGS
 else
-  ./configure --prefix="${INSTALL}/daemon"
+  ./configure $CONFIGURE_FLAGS --prefix="${INSTALL}/daemon"
 fi
 make -j$(nproc)
 make_install $global
@@ -73,13 +78,13 @@ fi
 make
 make_install $global
 
-cd "${TOP}/client-gnome"
+cd "${TOP}/${client}"
 mkdir -p ${BUILDDIR}
 cd ${BUILDDIR}
 if $global; then
   cmake .. $static
 else
-  cmake .. -DCMAKE_INSTALL_PREFIX="${INSTALL}/client-gnome" -DLibRingClient_DIR="${INSTALL}/lrc/lib/cmake/LibRingClient" $static
+  cmake .. -DCMAKE_INSTALL_PREFIX="${INSTALL}/${client}" -DLibRingClient_DIR="${INSTALL}/lrc/lib/cmake/LibRingClient" $static
 fi
 make
 make_install $global
