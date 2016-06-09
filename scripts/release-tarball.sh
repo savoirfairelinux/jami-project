95608454f80491bc3e0ebd19eee42cf89fca20f3
aviau <alexandre@alexandreviau.net>
2016-06-09 13:40:03 -0400

    Import release-tarball script from jenkins job
    This script comes from the jenkins jobs that publishes release tarballs
    on https://dl.ring.cx/ring-release/tarballs/. It is best to make it available
    in the source so that people can see how we generate our tarballs and maybe
    use it for other purposes.
    
    Change-Id: Ie0b302a562e92935ff97661fc85d31cdf20e0cf5
    Tuleap: #254

---
+++ scripts/release-tarball.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RING_PROJECT=$(realpath ${DIR}/..)
+
+# Download contrib tarballs
+cd ${RING_PROJECT}/daemon/contrib
+mkdir native
+cd native
+../bootstrap
+make fetch-all
+cd ..
+rm -rf native
+
+# Create tarball variables
+cd ${RING_PROJECT}
+lastcommitdate=`git log -1 --format=%cd --date=short` # YYYY-MM-DD
+numbercommits=`git log --format=%cd --date=short | grep -c $lastcommitdate` # number of commits that day
+dateshort=`echo $lastcommitdate | sed -s 's/-//g'` # YYYYMMDD
+commitid=`git rev-parse --short HEAD` # last commit id
+
+# Create tarball
+tmpdir=$(mktemp -d)
+cd ${tmpdir}
+tar -C ${RING_PROJECT}/.. --exclude-vcs -zcf ring_$dateshort.$numbercommits.$commitid.tar.gz $(basename ${RING_PROJECT})
+mv ring_$dateshort.$numbercommits.$commitid.tar.gz ${RING_PROJECT}
+
+# Cleanup contrib tarballs
+rm -rf ${RING_PROJECT}/daemon/contrib/tarballs/*
