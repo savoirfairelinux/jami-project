3b779f273d58d34ab82b19536c53d216ba9b8134
aviau <alexandre@alexandreviau.net>
2016-06-14 12:42:57 -0400

    ring-project packaging
    Change-Id: Id87a74a6c412ffc5bf82b9562639ce5a9424b06a

---
+++ scripts/build-package-debian.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# This script is used in the packagign containers to build packages on
+# debian-based distros.
+#
+
+set -e
+
+cp -r /opt/ring-project-ro /opt/ring-project
+cd /opt/ring-project
+
+# install build deps
+apt-get update
+mk-build-deps --remove --install debian/control -t "apt-get -y --no-install-recommends"
+
+# create changelog file
+dch --create --package ring --newversion ${RELEASE_VERSION}~dfsg1-1 "Automatic nightly release"
+dch --release --distribution "unstable" debian/changelog
+
+# create orig tarball
+mk-origtargz ${RELEASE_TARBALL_FILENAME}
+rm --verbose ${RELEASE_TARBALL_FILENAME}
+GET_ORIG_SOURCE_OVERRIDE_USCAN_TARBALL=$(realpath ../ring_*.orig.tar.gz) debian/rules get-orig-source
+
+# move the tarball to the work directory
+mkdir -p /opt/ring-packaging
+mv ring_*.orig.tar.gz /opt/ring-packaging
+
+# move to work directory
+cd /opt/ring-packaging
+
+# unpack the orig tarball
+tar -xvf /opt/ring-packaging/ring_*.orig.tar.gz
+
+# move to ring-project dir
+cd ring-project
+
+# import debian folder
+cp --verbose -r /opt/ring-project/debian .
+
+# create the package
+dpkg-buildpackage -uc -us
+
+# move the artifacts to output
+cd ..
+mv *.orig.tar* *.debian.tar* *deb *changes *dsc /opt/output
