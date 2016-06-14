5e70e7ae1164aea17d362764d53fbb8c09a59289
aviau <alexandre@alexandreviau.net>
2016-06-14 12:42:57 -0400

    Add Debian and Ubuntu packaging
    This moves the official Debian packaging into ring-project. Jenkins
    should now be able to run packaging jobs and publish packages. It
    should also make it easy to add a `verify` check on Jenkins to test
    packaging patches.
    
    This will make it much easier to work on packaging issues because the
    build environment is now reproductible on any machine.
    
    The patch adds Makefile targets to create packages such as:
     - package-all
     - package-<DISTRO>
    
    Builds are all done inside Docker containers and it is possible to
    build many at a time with the `package-all` target.
    
    To debug a build, use `package-<DISTRO>-interactive` to obtain a shell
    inside a container, ready to launch a build.
    
    Tuleap: #781
    Change-Id: Id87a74a6c412ffc5bf82b9562639ce5a9424b06a

---
+++ packaging/distros/README.rst
@@ -0,0 +1,6 @@
+Differences log
+###############
+
+This files lists the overrides done in the various subfolders. It is useful
+when updating packaging  because we don't maintain patches and we should know
+what was overriden and why.
---
+++ scripts/build-package-debian.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# This script is used in the packaging containers to build packages on
+# debian-based distros.
+#
+
+set -e
+
+cp -r /opt/ring-project-ro /opt/ring-project
+cd /opt/ring-project
+
+# import the debian folder and override files if needed
+cp -r packaging/rules/debian .
+if [ -z "${DEBIAN_PACKAGING_OVERRIDE}" ]; then
+    echo "DEBIAN_PACKAGING_OVERRIDE not set."
+else
+    cp -r ${DEBIAN_PACKAGING_OVERRIDE}/* debian/
+fi
+
+# install build deps
+apt-get update
+apt-get upgrade -y
+mk-build-deps --remove --install debian/control -t "apt-get -y --no-install-recommends"
+
+# create changelog file
+dch --create --package ring --newversion ${DEBIAN_VERSION} "Automatic nightly release"
+dch --release --distribution "unstable" debian/changelog
+
+# create orig tarball
+mk-origtargz ${RELEASE_TARBALL_FILENAME}
+rm --verbose ${RELEASE_TARBALL_FILENAME}
+GET_ORIG_SOURCE_OVERRIDE_USCAN_TARBALL=$(realpath ../ring_*.orig.tar.gz) debian/rules get-orig-source
+
+# move the tarball to the work directory
+mkdir -p /opt/ring-packaging
+mv ring_*.orig.tar.gz /opt/ring-packaging
+
+# move to work directory
+cd /opt/ring-packaging
+
+# unpack the orig tarball
+tar -xvf /opt/ring-packaging/ring_*.orig.tar.gz
+
+# move to ring-project dir
+cd ring-project
+
+# import debian folder into ring-packaging directory
+cp --verbose -r /opt/ring-project/debian .
+
+# create the package
+dpkg-buildpackage -uc -us
+
+# move the artifacts to output
+cd ..
+mv *.orig.tar* *.debian.tar* *deb *changes *dsc /opt/output
+chown -R ${CURRENT_UID}:${CURRENT_UID} /opt/output
