27da03e34c9859fbb078a2ac2a305e7f6f24bbe2
David Côté-Tremblay <david.cote-tremblay@savoirfairelinux.com>
2016-10-25 15:29:36 -0400

    Change GNUTLS dependencies from libgnutls-dev to libgnutls28-dev (because libgnutls-dev does not exists in official repos)
    Change-Id: Ib151d56cca6541b3f5eac797d30d808dc54a4604

--- make-ring.py
+++ make-ring.py
@@ -120,7 +120,7 @@ DEBIAN_DEPENDENCIES = [
     'gnome-icon-theme-symbolic', 'libasound2-dev', 'libavcodec-dev',
     'libavcodec-extra', 'libavdevice-dev', 'libavformat-dev', 'libboost-dev',
     'libclutter-gtk-1.0-dev', 'libcppunit-dev', 'libdbus-1-dev',
-    'libdbus-c++-dev', 'libebook1.2-dev', 'libexpat1-dev', 'libgnutls-dev',
+    'libdbus-c++-dev', 'libebook1.2-dev', 'libexpat1-dev', 'libgnutls28-dev',
     'libgsm1-dev', 'libgtk-3-dev', 'libjack-dev', 'libnotify-dev',
     'libopus-dev', 'libpcre3-dev', 'libpulse-dev', 'libsamplerate0-dev',
     'libsndfile1-dev', 'libspeex-dev', 'libspeexdsp-dev', 'libswscale-dev', 'libtool',
