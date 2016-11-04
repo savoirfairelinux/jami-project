ffa666366acc447cf1e6a6c23163b4bf901baab6
aviau <alexandre@alexandreviau.net>
2016-11-04 02:13:53 -0400

    Packaging: ubuntu 16.10 support
    Change-Id: I78080e5a9be8260bc4e855424e7c5045c808cc09

--- scripts/build-package-debian.sh
+++ scripts/build-package-debian.sh
@@ -46,7 +46,7 @@ dch --release --distribution "unstable" debian/changelog
 
 # create orig tarball
 # mk-origtargz isn't in ubuntu_14.04
-if [ "${DISTRIBUTION}" = "ubuntu_14.04" ] || [ "${DISTRIBUTION}" = "ubuntu_14.04_i386" ]; then
+if [ "${DISTRIBUTION}" = "ubuntu_14.04" ] || [ "${DISTRIBUTION}" = "ubuntu_14.04_i386" ];
     mv ${RELEASE_TARBALL_FILENAME} ../ring_${DEBIAN_VERSION}.orig.tar.gz
 else
     mk-origtargz ${RELEASE_TARBALL_FILENAME}
