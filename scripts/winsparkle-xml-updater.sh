1402ecf7a4fd9e00f4a3a1f4528238f14683cd38
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2016-11-08 10:33:20 -0500

    add winsparkle xml updated script
    This script is needed to update a Windows Sparkle XML file
    for Windows auto-update.
    
    The script has been modified to take account of the new repository
    dl.ring.cx.
    It also requires gawk for inplace modifications.
    
    Change-Id: I4ca9b3fcc126e22db3b5303fabda3c666ef2f7f1

---
+++ scripts/winsparkle-xml-updater.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Update SPARKLE_FILE with given executable
+# Usage ./winsparkle-xml-updater.sh ring.exe <URI of winsparkle-ring.xml>
+
+PACKAGE=$1
+SPARKLE_SOURCE=$2
+SPARKLE_FILE=winsparkle-ring.xml
+TMP_FILE=winsparkle.tmp
+REPO_URL=${2%/${SPARKLE_FILE}}
+
+
+if [ ! -f ${PACKAGE} ]; then
+    echo "Can't find package aborting..."
+    exit 1
+fi
+
+if [ ! -s ${SPARKLE_FILE} ]; then
+    wget --no-check-certificate --tries=10 -q ${SPARKLE_SOURCE} -O ${SPARKLE_FILE}
+    if [ $? -ne 0 ]; then
+        rm -f winsparkle.xml
+        exit 1
+    fi
+fi
+
+if [ -f ${SPARKLE_FILE} ]; then
+    ITEMS=$(sed -n "/<item>/,/<\/item>/p" ${SPARKLE_FILE})
+fi
+
+if [[ $(basename ${PACKAGE}) == *"x86_64"* ]]
+then
+    OS="windows-x64";
+else
+    OS="windows-x86";
+fi
+
+cat << EOS > ${TMP_FILE}
+        <item>
+            <title>Ring nightly $(date "+%Y/%m/%d %H:%M")</title>
+            <pubDate>$(date -R)</pubDate>
+            <enclosure url="${REPO_URL}/$(basename ${PACKAGE})" sparkle:version="$(date +%Y%m%d)" sparkle:shortVersionString="nightly-$(date "+%Y%m%d")" sparkle:os="${OS}" length="$(stat -c %s ${PACKAGE})" type="application/octet-stream" />
+        </item>
+EOS
+
+if [ -s ${SPARKLE_FILE} ];then
+    gawk -i inplace -v tmp="${TMP_FILE}" '/language/{print; while(getline line < tmp){print line};close(tmp);next}1' ${SPARKLE_FILE}
+else
+    echo 'empty SPARKLE_FILE'
+fi
