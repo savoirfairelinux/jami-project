87558adb3456a40b858a817cabc64d20ea30d573
aviau <alexandre@alexandreviau.net>
2016-12-07 13:59:22 -0500

    Packaging: Add Fedora25
    Change-Id: Ifd9edfa8dcfaae7c657f28b84d09fe39d98aaedd

--- scripts/make-packaging-target.py
+++ scripts/make-packaging-target.py
@@ -181,6 +181,16 @@ def run_generate_all(parsed_args):
             "debian_packaging_override": "",
             "output_file": ".packages-built",
         },
+        {
+            "distribution": "fedora_25",
+            "debian_packaging_override": "",
+            "output_file": ".packages-built",
+        },
+        {
+            "distribution": "fedora_25_i386",
+            "debian_packaging_override": "",
+            "output_file": ".packages-built",
+        },
         # Gentoo
         {
             "distribution": "gentoo",
