43e849e2f5225045a81ef4adaa72446434cd18e3
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-07-24 15:58:03 -0400

    packaging: Debian packaging update
    Backport various fixes from Alexandre's work on the official Debian
    package:
    
    Debian packaging:
    * Add missing libqt5sql5-sqlite dependency
    * Various copyright updates
    * Update Standards-Version
    * Priority optional (extra is deprecated now)
    * Update Vcs-* fields
    * Add missing copyright entry
    * Remove useless gbp.conf file (only meaningful when using
      git-buildpackage in a git-buildpackage compatible repo which is
      not the case here)
    
    Packaging scripts:
    * Remove Ubuntu 14.04 and 15.10 related code
    * Various copyright updates
    * Set e-mail address before generating changelog entry
    
    Change-Id: I18e523422d16fe129cf6773ab01a99b00232a5bb
    Gitlab: #481
    Reviewed-by: Sebastien Blin <sebastien.blin@savoirfairelinux.com>

--- scripts/ubuntu-15.10-android-install-all-devices.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Build the Android app and install it on all connected devices.
-set -e
-./ubuntu-15.10-dependencies.sh
-cd client-android
-./compile.sh
-# TODO use gradlew instead.
-# From: http://stackoverflow.com/questions/8610733/how-can-i-adb-install-an-apk-to-multiple-connected-devices
-adb devices | tail -n +2 | cut -sf 1 | xargs -i'{}' adb -s '{}' install ring-android/app/build/outputs/apk/app-debug.apk
