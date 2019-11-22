78e4ff9c6fb69a7ae1c6d6eb57e848361df31fa8
Igor Stegarescu <igor.stegarescu@savoirfairelinux.com>
2019-11-22 13:03:31 -0500

    packaging: rpm rhel8 one click install
    Change-Id: Ifebc562291c87a78a597ad93323a4693d8d4127b

--- scripts/make-packaging-target.py
+++ scripts/make-packaging-target.py
@@ -356,6 +356,12 @@ def run_generate_all(parsed_args):
             "output_file": ".packages-built",
             "options": "--security-opt seccomp=./docker/profile-seccomp-fedora_28.json --privileged",
         },
+        {
+            "distribution": "rhel_one-click-install_8",
+            "debian_packaging_override": "",
+            "output_file": ".packages-built",
+            "options": "--security-opt seccomp=./docker/profile-seccomp-fedora_28.json --privileged",
+        },
 
         # Gentoo
         {
