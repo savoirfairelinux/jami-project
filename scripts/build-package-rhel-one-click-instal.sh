f7564e9a50c35ea402c74a6aaca15e000a89ebf6
Igor Stegarescu <igor.stegarescu@savoirfairelinux.com>
2019-11-22 13:03:31 -0500

    packaging: rpm rhel8 one click install
    Change-Id: Ifebc562291c87a78a597ad93323a4693d8d4127b

--- scripts/build-package-rhel-one-click-instal.sh
+++ scripts/build-package-rhel-one-click-instal.sh
@@ -23,36 +23,24 @@
 
 set -x
 
-# import the spec file
-mkdir -p /opt/ring-project
-cd /opt/ring-project
-cp /opt/ring-project-ro/packaging/rules/rhel/* .
-
 #create tree for build
 rpmdev-setuptree
 
+# import the spec file
+mkdir -p /opt/ring-project
+cd /opt/ring-project
+cp /opt/ring-project-ro/packaging/rules/rhel-one-click-install/* .
+
 # place the source
-#mkdir -p /root/rpmbuild/SOURCES
+mkdir -p /root/rpmbuild/SOURCES
 cp /opt/ring-project-ro/jami_*.tar.gz /root/rpmbuild/SOURCES
 
 # Set the version
-sed -i "s/RELEASE_VERSION/${RELEASE_VERSION}/g" *.spec
-rpmdev-bumpspec --comment="Automatic nightly release" --userstring="Jenkins <ring@lists.savoirfairelinux.net>" jami.spec
+sed -i "s/RELEASE_VERSION/${RELEASE_VERSION}/g" jami-one-click.spec
 rpmdev-bumpspec --comment="Automatic nightly release" --userstring="Jenkins <ring@lists.savoirfairelinux.net>" jami-one-click.spec
 
-
 # install build deps
-dnf builddep -y jami.spec || echo "ignoring dnf builddep failure"
-
-# build the package
-rpmbuild -ba jami.spec
-
-# move to output
-mv /root/rpmbuild/RPMS/*/* /opt/output
-touch /opt/output/.packages-built
-chown -R ${CURRENT_UID}:${CURRENT_UID} /opt/output
-
-## JAMI ONE CLICK INSTALL RPM
+dnf builddep -y jami-one-click.spec || echo "ignoring dnf builddep failure"
 
 #copy script jami-all.postinst which add repo 
 mkdir -p /root/rpmbuild/BUILD/ring-project/packaging/rules/rhel-one-click-install/
@@ -66,5 +54,3 @@ mkdir -p /opt/output/rhel-one-click-install
 mv /root/rpmbuild/RPMS/*/* /opt/output/rhel-one-click-install
 touch /opt/output/rhel-one-click-install/.packages-built
 chown -R ${CURRENT_UID}:${CURRENT_UID} /opt/output/rhel-one-click-install
-
-
