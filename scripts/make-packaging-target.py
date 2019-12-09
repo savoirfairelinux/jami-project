73242e1f0860007aeeb75690e7c87c58c7648760
Fredy P <fredy.pulido@savoirfairelinux.com>
2019-12-09 10:15:45 -0500

    packaging: adds debian 10 arm64
    Warning: not fully functional, require companion changes in daemon
    to ensure the right compilation flags
    
    Change-Id: I905bbd4660824670d7de836129886f3d7f1dc83b

--- scripts/make-packaging-target.py
+++ scripts/make-packaging-target.py
@@ -138,6 +138,12 @@ def run_generate_all(parsed_args):
             "options": "--privileged --security-opt apparmor=docker-default",
         },
         {
+            "distribution": "debian_10_arm64",
+            "debian_packaging_override": "",
+            "output_file": "$(DEBIAN_DSC_FILENAME)",
+            "options": "--privileged --security-opt apparmor=docker-default",
+        },
+        {
             "distribution": "debian_10_oci",
             "docker_image": "debian_10",
             "debian_packaging_override": "",
