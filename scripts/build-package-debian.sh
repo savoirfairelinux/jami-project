ab46408852ce1033d064087239f8efb99afa3605
Sandra Tobajas <sandra.tobajas@savoirfairelinux.com>
2020-10-08 11:18:56 +0000

    packaging: debian: use cross compilation for armhf platforms
    Using qemu in user mode to build jami package for raspbian platforms
    adds a lot of overhead.
    
    Remove the virtualization using the Debian 10 cross building tools. The
    host platform (i.e. the platform on which the package will be run) has
    to be set and some compilation options has to be added in the
    debian/rules file.
    
    Also add the armhf architecture in the Docker container in order to
    install the package dependencies for this platform.
    
    Change-Id: Ic2282ad9377d52482bca4a4f0ef017fbcc1f8eb7

--- scripts/build-package-debian.sh
+++ scripts/build-package-debian.sh
@@ -21,7 +21,7 @@
 # debian-based distros.
 #
 
-set -e
+set -ex
 
 cp -r /opt/ring-project-ro /opt/ring-project
 cd /opt/ring-project
@@ -39,6 +39,16 @@ if [ -z "${DEBIAN_PACKAGING_OVERRIDE}" ]; then
     echo "DEBIAN_PACKAGING_OVERRIDE not set."
 else
     cp -r ${DEBIAN_PACKAGING_OVERRIDE}/* debian/
+fi
+
+DPKG_BUILD_OPTIONS=""
+MKBUILD_OPTIONS=""
+# Set the host architecture as armhf
+if echo "${DISTRIBUTION}" | grep -q "raspbian_10_armhf"; then
+    echo "Adding armhf host architecture"
+    dpkg --add-architecture armhf
+    DPKG_BUILD_OPTIONS="${DPKG_BUILD_OPTIONS} -a armhf"
+    MKBUILD_OPTIONS="${MKBUILD_OPTIONS} --host-arch armhf"
 fi
 
 # install build deps
@@ -74,7 +84,7 @@ cd ring-project
 cp --verbose -r /opt/ring-project/debian .
 
 # create the package
-dpkg-buildpackage -uc -us
+dpkg-buildpackage -uc -us ${DPKG_BUILD_OPTIONS}
 
 # move the artifacts to output
 cd ..
