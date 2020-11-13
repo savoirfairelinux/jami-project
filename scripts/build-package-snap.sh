c6fcbf9b6f312f7a81efe8b600f5e386bae28e34
Amin Bandali <amin.bandali@savoirfairelinux.com>
2020-11-13 10:33:37 -0500

    packaging: add snap
    Change-Id: I5b24b809cec3763c44808fe9f056cba862336531

---
+++ scripts/build-package-snap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2020 Savoir-faire Linux Inc.
+#
+# Author: Amin Bandali <amin.bandali@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This script is used in the packaging containers to build a snap
+# package on an ubuntu base distro.
+
+
+set -e
+
+# import the snapcraft file
+mkdir -p /opt/ring-project
+cd /opt/ring-project
+cp -rp /opt/ring-project-ro/packaging/rules/snap/* .
+
+# set the version and tarball filename
+sed -i "s/RELEASE_VERSION/${RELEASE_VERSION}/g" snapcraft.yaml
+sed -i "s/RELEASE_TARBALL_FILENAME/${RELEASE_TARBALL_FILENAME}/g" snapcraft.yaml
+
+snapcraft
+
+# move the built snap to output
+mv *.snap /opt/output/
