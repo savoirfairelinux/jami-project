97147cee0717ef5fbdb363d427d0eaed3a644234
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-11 10:24:11 -0500

    swarm: christmas edition package
    Change-Id: I785b8f8bfe10e681de6456058a786e4cb34ca644

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,16 +17,23 @@ set -ex
 QT5_MIN_VER="5.14"
 
 global=false
+appimage=''
 static=''
 client=''
 qt5ver=''
 qt5path=''
+subversion=''
 proc='1'
 priv_install=true
-while getopts gsc:q:Q:P:p:u OPT; do
+while getopts gsc:q:v:Q:P:p:u:a OPT; do
   case "$OPT" in
     g)
       global='true'
+    ;;
+    a)
+      appimage='-DENABLE_LIBWRAP=true'
+      static='-DENABLE_STATIC=true'
+      priv_install='false'
     ;;
     s)
       static='-DENABLE_STATIC=true'
@@ -45,6 +52,9 @@ while getopts gsc:q:Q:P:p:u OPT; do
     ;;
     p)
       proc="${OPTARG}"
+    ;;
+    v)
+      subversion="${OPTARG}"
     ;;
     u)
       priv_install='false'
@@ -86,12 +96,12 @@ if [ "${prefix+set}" ]; then
 else
     ../bootstrap
 fi
-make
+make -j"${proc}"
 cd "${DAEMON}"
 ./autogen.sh
 
 #keep shared Lib on MAC OSX
-if [[ "$OSTYPE" != "darwin"* ]]; then
+if [ "$OSTYPE" != "darwin"* ] && [ ! "$appimage" ] ; then
     sharedLib="--disable-shared"
 fi
 
@@ -143,12 +153,12 @@ if [ "${global}" = "true" ]; then
     cmake .. -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" \
              -DCMAKE_BUILD_TYPE=Debug \
              -DCMAKE_INSTALL_PREFIX="${prefix}" $static \
-             -DQT5_VER="${qt5ver}" \
+             -DQT5_VER="${qt5ver}" $appimage \
              -DQT5_PATH="${qt5path}"
   else
     cmake .. -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" \
              -DCMAKE_BUILD_TYPE=Debug $static \
-             -DQT5_VER="${qt5ver}" \
+             -DQT5_VER="${qt5ver}" $appimage \
              -DQT5_PATH="${qt5path}"
   fi
 else
@@ -156,7 +166,7 @@ else
             -DCMAKE_BUILD_TYPE=Debug \
             -DCMAKE_INSTALL_PREFIX="${INSTALL}/lrc" \
             -DRING_BUILD_DIR="${DAEMON}/src" $static \
-            -DQT5_VER="${qt5ver}" \
+            -DQT5_VER="${qt5ver}" $appimage \
             -DQT5_PATH="${qt5path}"
 fi
 make -j"${proc}"
@@ -201,4 +211,9 @@ if [ -z ${qt5path}]; then
   python ../copy-runtime-files.py
 else
   python ../copy-runtime-files.py -q ${qt5path}
+fi
+
+if [ "$appimage" ]; then
+  cd ..
+  sh ./packaging/package-appimage.sh ${subversion}
 fi
