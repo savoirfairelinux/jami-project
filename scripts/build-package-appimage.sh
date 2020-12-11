74febead4251d374fa73b1c2c6a6f6a4eefc2c8f
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-11 10:24:11 -0500

    swarm: christmas edition package
    Change-Id: I785b8f8bfe10e681de6456058a786e4cb34ca644

---
+++ scripts/build-package-appimage.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2020 Savoir-faire Linux Inc.
+#
+# Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This script is used in the packaging containers to build a snap
+# package on an ubuntu base distro.
+
+set -e
+
+cp -r /opt/ring-project-ro /opt/ring-project
+cd /opt/ring-project
+
+./make-ring.py --init
+./make-ring.py --dependencies --qt
+./make-ring.py --install --qt --appimage
