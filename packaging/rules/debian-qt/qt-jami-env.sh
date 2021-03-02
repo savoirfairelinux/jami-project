aede5304a6439951695b146db6cdc5433bf7ffba
Amin Bandali <amin.bandali@savoirfairelinux.com>
2021-03-02 10:48:00 -0500

    debian: build and package our own Qt
    Change-Id: I983607f226476c32a6862a41b55ca1b13a2c010b

---
+++ packaging/rules/debian-qt/qt-jami-env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export PATH=/usr/lib/qt-jami/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/lib/qt-jami:${LD_LIBRARY_PATH}
+export PKG_CONFIG_PATH=/usr/lib/qt-jami/pkgconfig:${PKG_CONFIG_PATH}
