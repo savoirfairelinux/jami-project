11679b8dad26568e7d74905da87d9dc3acb4931d
Maxim Cournoyer <maxim.cournoyer@savoirfairelinux.com>
2021-03-08 15:55:37 -0500

    Jenkinsfile: Move the Guix environment specification to a manifest file.
    This makes the Jenkinsfile cleaner and allows developers to easily
    reproduce the same environment.
    
    * guix/minimal-manifest.scm: New file.
    * Jenkinsfile ('Generate release tarball): Use it to spawn the
    container used to build the release tarball.  Also expose /usr/bin/env
    in the container.  Use the new 'portable-release-tarball' target to
    simplify things.  Fix the shebang line; it needs to be the first line
    of the script.
    
    GitLab: https://git.jami.net/savoirfairelinux/jami-packaging/-/issues/98
    Change-Id: I2b66cb0d54c3aa5f1f95ed2b499a370c935e0db6

---
+++ guix/minimal-manifest.scm
@@ -0,0 +1,25 @@
+;;; To use with the GNU Guix package manager.
+;;; Available at https://guix.gnu.org/.
+;;;
+;;; Commentary:
+;;;
+;;; This Guix manifest can be used to create an environment that
+;;; satisfies the minimal requirements of the the contrib build system
+;;; of the daemon.  For example, it is used by the CI to build the
+;;; source release tarball in a controlled environment.
+
+(specifications->manifest
+ (list
+  "coreutils"
+  "gcc-toolchain"
+  "git-minimal"
+  "grep"
+  "gzip"
+  "make"
+  "nss-certs"
+  "pkg-config"
+  "python"
+  "sed"
+  "tar"
+  "wget"
+  "xz"))
