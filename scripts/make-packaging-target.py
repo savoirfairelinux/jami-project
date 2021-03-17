c977902a53bb3eac7a330b655fc71f92e2bc4f93
Amin Bandali <amin.bandali@savoirfairelinux.com>
2021-03-17 10:40:22 -0400

    debian: build and package client-qt
    Change-Id: Ida109a811bebd1fb46fd1a8b90fb5463518382c0

--- scripts/make-packaging-target.py
+++ scripts/make-packaging-target.py
@@ -122,7 +122,7 @@ def run_generate_all(parsed_args):
             "distribution": "debian_10",
             "debian_packaging_override": "",
             "output_file": "$(DEBIAN_DSC_FILENAME)",
-            "options": "--privileged --security-opt apparmor=docker-default",
+            "options": "-e QTDIR=$(QT_JAMI_BASE_DIR) -e QTVER=$(QT_MAJOR).$(QT_MINOR).$(QT_PATCH) --privileged --security-opt apparmor=docker-default",
         },
         {
             "distribution": "debian_10_i386",
