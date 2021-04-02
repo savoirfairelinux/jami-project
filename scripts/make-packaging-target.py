d6d5cb85ccb372f2318fd5a332cee9818d88aea6
Amin Bandali <amin.bandali@savoirfairelinux.com>
2021-04-02 11:20:28 -0400

    ubuntu: build and package our own Qt
    Change-Id: I87934155fc43e9d9e602e838380ed5733e2f15f5

--- scripts/make-packaging-target.py
+++ scripts/make-packaging-target.py
@@ -89,7 +89,18 @@ RPM_BASED_SYSTEMS_DOCKER_RUN_OPTIONS = (
     '--privileged')
 
 
-def generate_target(distribution, output_file, options='', docker_image='', version='', docker_build_args = ''):
+DPKG_BASED_SYSTEMS_DOCKER_RUN_OPTIONS_QT = (
+    '-e QT_JAMI_PREFIX=$(QT_JAMI_PREFIX) '
+    '-e QT_MAJOR=$(QT_MAJOR) '
+    '-e QT_MINOR=$(QT_MINOR) '
+    '-e QT_PATCH=$(QT_PATCH) '
+    '-e QT_TARBALL_CHECKSUM=$(QT_TARBALL_CHECKSUM) '
+    '-v /opt/ring-contrib:/opt/ring-contrib '
+    '--privileged --security-opt apparmor=docker-default')
+
+
+def generate_target(distribution, output_file, options='', docker_image='',
+                    version='', docker_build_args=''):
     if (docker_image == ''):
         docker_image = distribution
     if (version == ''):
@@ -138,13 +149,7 @@ def run_generate_all(parsed_args):
         {
             "distribution": "debian_10_qt",
             "output_file": "$(DEBIAN_QT_DSC_FILENAME)",
-            "options": ('-e QT_JAMI_PREFIX=$(QT_JAMI_PREFIX) '
-                        '-e QT_MAJOR=$(QT_MAJOR) '
-                        '-e QT_MINOR=$(QT_MINOR) '
-                        '-e QT_PATCH=$(QT_PATCH) '
-                        '-e QT_TARBALL_CHECKSUM=$(QT_TARBALL_CHECKSUM) '
-                        '-v /opt/ring-contrib:/opt/ring-contrib '
-                        '--privileged --security-opt apparmor=docker-default'),
+            "options": ,
             "version": "$(DEBIAN_QT_VERSION)",
         },
         {
@@ -198,6 +203,18 @@ def run_generate_all(parsed_args):
             "output_file": "$(DEBIAN_DSC_FILENAME)",
         },
         {
+            "distribution": "ubuntu_18.04_qt",
+            "output_file": "$(DEBIAN_QT_DSC_FILENAME)",
+            "options": DPKG_BASED_SYSTEMS_DOCKER_RUN_OPTIONS_QT,
+            "version": "$(DEBIAN_QT_VERSION)",
+        },
+        {
+            "distribution": "ubuntu_18.04_qt_i386",
+            "output_file": "$(DEBIAN_QT_DSC_FILENAME)",
+            "options": DPKG_BASED_SYSTEMS_DOCKER_RUN_OPTIONS_QT,
+            "version": "$(DEBIAN_QT_VERSION)",
+        },
+        {
             "distribution": "ubuntu_18.04_oci",
             "docker_image": "ubuntu_18.04",
             "output_file": "$(DEBIAN_OCI_DSC_FILENAME)",
@@ -217,6 +234,12 @@ def run_generate_all(parsed_args):
             "options": "--privileged --security-opt apparmor=docker-default",
         },
         {
+            "distribution": "ubuntu_20.04_qt",
+            "output_file": "$(DEBIAN_QT_DSC_FILENAME)",
+            "options": DPKG_BASED_SYSTEMS_DOCKER_RUN_OPTIONS_QT,
+            "version": "$(DEBIAN_QT_VERSION)",
+        },
+        {
             "distribution": "ubuntu_20.04_oci",
             "docker_image": "ubuntu_20.04",
             "output_file": "$(DEBIAN_OCI_DSC_FILENAME)",
@@ -227,6 +250,12 @@ def run_generate_all(parsed_args):
             "distribution": "ubuntu_20.10",
             "output_file": "$(DEBIAN_DSC_FILENAME)",
             "options": "--privileged --security-opt apparmor=docker-default",
+        },
+        {
+            "distribution": "ubuntu_20.10_qt",
+            "output_file": "$(DEBIAN_QT_DSC_FILENAME)",
+            "options": DPKG_BASED_SYSTEMS_DOCKER_RUN_OPTIONS_QT,
+            "version": "$(DEBIAN_QT_VERSION)",
         },
         {
             "distribution": "ubuntu_20.10_oci",
