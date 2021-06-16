6a9994fdc7c0a2f48418082ce890e59f6ea55312
Maxim Cournoyer <maxim.cournoyer@savoirfairelinux.com>
2021-06-16 13:57:43 -0400

    packaging: Temporarily disable building jami-gnome, as it's broken.
    The jami-gnome requires to be adjusted following the recent swarm
    changes merged, and doesn't currently build.  Disable it until it's
    fixed.
    
    * packaging/rules/debian/rules: Comment out the blocks
    configuring/building client-gnome.
    * scripts/build-package-rpm.sh (QT_PATCH): Comment out jami-gnome.spec.
    
    Change-Id: Ife278d7e1df62e18000f84327d0a534c898b1a04

--- scripts/build-package-rpm.sh
+++ scripts/build-package-rpm.sh
@@ -112,7 +112,7 @@ rpmbuild -ba jami-libclient.spec
 rpm --install /root/rpmbuild/RPMS/x86_64/jami-libclient-*
 
 # Build the GNOME and Qt clients.
-rpmbuild -ba jami-gnome.spec jami-qt.spec
+rpmbuild -ba jami-qt.spec #jami-gnome.spec
 
 # Move the built packages to the output directory.
 mv /root/rpmbuild/RPMS/*/* /opt/output
