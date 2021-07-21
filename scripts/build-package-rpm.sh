3d8a18593db18b267fa1b35dfd1640eaf8aff7ba
Maxim Cournoyer <maxim.cournoyer@savoirfairelinux.com>
2021-07-21 08:46:19 -0400

    Makefile: Repatriate the definition of packaging targets.
    Prior to this change, the Makefile packaging targets were generated by
    a Python script, supposedly to reduce boilerplate.  Make is able to
    programmatically define rules, so use its features instead.
    
    The packaging targets 'package-' prefix is dropped, making the targets
    names match the distribution directly, such as 'ubuntu-20.04'.  Only
    hyphens are used in the package target names, making it easier to
    remember.
    
    The docker *-ro bind mounts are dropped; instead only the release
    tarball file name is exposed to the containers as *the* source, which
    makes it unambiguous as to which sources were used.
    
    The files related to RHEL builds are removed, as the RHEL build is
    known to be broken and isn't being used.
    
    * Makefile (DISTRIBUTIONS): New variable.
    (make-docker-package-target): New function.
    (docker/Dockerfile-snap): Adjust accordingly.
    docker/*: Standardize to use hyphens instead of a mix of hyphens and
    underscores.
    * docker/Dockerfile_rhel_8: Delete file.
    * docker/profile-seccomp-fedora_28.json: Delete file.
    * scripts/make-packaging-target.py: Delete file.
    * Jenkinsfile: Adjust accordingly.
    
    Change-Id: I7f355b21ac7fbb009d55883c0907b98608bba3a7
    
    Change-Id: I2dc42b489a54b177ad038398a9d23a2a7e3007ec

--- scripts/build-package-rpm.sh
+++ scripts/build-package-rpm.sh
@@ -27,14 +27,17 @@ set -e
 # Import the spec file.
 mkdir -p /opt/ring-project
 cd /opt/ring-project
-cp /opt/ring-project-ro/packaging/rules/rpm/* .
+
+tar xf "/src/$RELEASE_TARBALL_FILENAME" ring-project/packaging/rules/rpm \
+    --strip-components=3 && mv rpm/* . && rmdir rpm
+
 rm jami-libqt.spec
 
 # Prepare the build tree.
 rpmdev-setuptree
 
 # Copy the source tarball.
-cp /opt/ring-project-ro/jami_*.tar.gz /root/rpmbuild/SOURCES
+cp --reflink=auto "/src/$RELEASE_TARBALL_FILENAME" /root/rpmbuild/SOURCES
 
 QT_JAMI_PREFIX="/usr/lib64/qt-jami"
 PATH="${QT_JAMI_PREFIX}/bin:${PATH}"
@@ -66,7 +69,9 @@ if [[ "${DISTRIBUTION:0:4}" == "rhel" \
     if [ ! -f "${RPM_PATH}" ]; then
         mkdir /opt/qt-jami-build
         cd /opt/qt-jami-build
-        cp /opt/ring-project-ro/packaging/rules/rpm/jami-libqt.spec .
+        tar xf "/src/$RELEASE_TARBALL_FILENAME" \
+            ring-project/packaging/rules/rpm/jami-libqt.spec \
+            --strip-components=4 .
 
         # Fetch and cache the tarball, if not already available.
         if [ ! -f "$CACHED_QT_TARBALL" ]; then
