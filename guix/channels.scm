74be5d90ae042db8424209eac5c69a4d41f17bea
Maxim Cournoyer <maxim.cournoyer@savoirfairelinux.com>
2022-01-10 15:07:50 -0500

    build: Use guix shell.
    The recently introduced 'guix shell' command automatically manages a
    cached profile for us; prefer it to 'guix environment', and do away
    with a channels file, given fixes in Guix have now been incorporated
    in the main branch.
    
    * Makefile (portable-release-tarball)<guix environment>: Replace by
    'guix shell'.
    * build.py (run_dependencies)[guix]: Likewise, and do not use a
    channels.scm file.
    (run_install): Likewise, and provide a hint when TARBALLS is not set.
    * guix/channels.scm: Delete file.
    * guix/manifest.scm: Adjust comment.
    
    Change-Id: I50ca99e51f0ebca136c9bbc5a0ca3063075432b4

--- guix/channels.scm
+++
@@ -1,8 +0,0 @@
-(list (channel
-       (inherit %default-guix-channel)
-       ;; Use the staging branch for now, as it includes more debug
-       ;; symbols and fixes a propagation conflict between
-       ;; gdk-pixbuf+svg and gdk-pixbuf.
-       (branch "staging")
-       (commit
-        "42231bc15df441d6426dec57283aca9ae7a03fcf")))
