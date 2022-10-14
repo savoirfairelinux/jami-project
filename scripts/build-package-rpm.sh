1655a66cf322ea90afd9666a5761ab49ad87a592
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-10-14 16:37:40 -0400

    packaging: add fedora 37
    Change-Id: Iec616c3ae7c33c9c443954e15a8c5a2c2d63f964

--- scripts/build-package-rpm.sh
+++ scripts/build-package-rpm.sh
@@ -113,6 +113,8 @@ if [ ! -f "${RPM_PATH}" ]; then
             cp /root/rpmbuild/RPMS/x86_64/jami-libqt-$QT_MAJOR_MINOR_PATCH-*.fc35.x86_64.rpm "${RPM_PATH}"
         elif [[ "${DISTRIBUTION}" == "fedora_36" ]]; then
             cp /root/rpmbuild/RPMS/x86_64/jami-libqt-$QT_MAJOR_MINOR_PATCH-*.fc36.x86_64.rpm "${RPM_PATH}"
+        elif [[ "${DISTRIBUTION}" == "fedora_37" ]]; then
+            cp /root/rpmbuild/RPMS/x86_64/jami-libqt-$QT_MAJOR_MINOR_PATCH-*.fc37.x86_64.rpm "${RPM_PATH}"
         else
             cp /root/rpmbuild/RPMS/x86_64/jami-libqt-*.rpm "${RPM_PATH}"
         fi
